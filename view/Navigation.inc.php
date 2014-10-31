ba96d58b318164dae4ab89e57ffe1372845ccefc
Yorickdewid <yorick17@outlook.com>
2014-10-31 17:58:25 +0100

    Add active links to navigation

--- view/Navigation.inc.php
+++ view/Navigation.inc.php
@@ -5,16 +5,16 @@
     <li <? echo(getRoute()=="api"?"class=\"current-menu-item\"":""); ?>><a href="/api">API</a></li>
     <li <? echo(getRoute()=="get-involved"?"class=\"current-menu-item\"":""); ?>><a href="/get-involved">GET INVOLVED</a>
       <ul>
-        <li><a href="/contribute">CONTRIBUTE</a></li>
-        <li><a href="/submit">SUBMIT</a></li>
+        <li <? echo(getRoute()=="contribute"?"class=\"current-menu-item\"":""); ?>><a href="/contribute">CONTRIBUTE</a></li>
+        <li <? echo(getRoute()=="submit"?"class=\"current-menu-item\"":""); ?>><a href="/submit">SUBMIT</a></li>
       </ul>
     </li>
-    <li><a href="/blog">BLOG</a></li>
-    <li><a href="/contact">SUPPORT</a>
+    <li <? echo(getRoute()=="blog"?"class=\"current-menu-item\"":""); ?>><a href="/blog">BLOG</a></li>
+    <li <? echo(getRoute()=="support"?"class=\"current-menu-item\"":""); ?>><a href="/support">SUPPORT</a>
       <ul>
-        <li><a href="/faq">FAQ</a></li>
-        <li><a href="/contact">CONTACT</a></li>
-        <li><a href="/about">ABOUT</a></li>
+        <li <? echo(getRoute()=="faq"?"class=\"current-menu-item\"":""); ?>><a href="/faq">FAQ</a></li>
+        <li <? echo(getRoute()=="contact"?"class=\"current-menu-item\"":""); ?>><a href="/contact">CONTACT</a></li>
+        <li <? echo(getRoute()=="about"?"class=\"current-menu-item\"":""); ?>><a href="/about">ABOUT</a></li>
       </ul>
     </li>
   </ul>
