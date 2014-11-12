822ca5808f591300ef6b2f6aee1e33a9aebd4c28
Yorickdewid <yorick17@outlook.com>
2014-11-12 23:35:00 +0100

    Add User model

---
+++ class/User.class.php
@@ -0,0 +1,110 @@
+<?php
+
+requireClass("Postgres");
+
+class User {
+	var $id;
+	var $username;
+	var $_password;
+	var $email;
+	var $email_confirmed;
+	var $blocked;
+	var $reputation;
+	var $note;
+	var $created;
+	var $timestamp;
+
+	private $_conn = NULL;
+
+	function __construct($username, $password, $email){
+		$this->username = $username;
+		$this->_password = $password;
+		$this->email = $email;
+		$this->_conn = new Postgres("opendb", "localhost", 5432, "opendb", "q");
+	}
+
+	function __destruct(){
+		$this->_conn->DBClose();
+	}
+
+	private function _insert(){
+		if(!empty($this->username) && !empty($this->_password) && !empty($this->email)){
+			$query = sprintf("INSERT INTO public.user (username, password, email) VALUES ('%s', '%s', '%s') RETURNING userid", $this->username, $this->_password, $this->email);
+			$this->_conn->ExecQuery($query);
+			$rs = $this->_conn->FetchResult();
+			return $rs['userid'];
+		}
+	}
+
+	private function _get(){
+		if(!empty($this->id)){
+			$query = sprintf("SELECT * FROM public.user WHERE userid=%d", $this->id);
+			$this->_conn->ExecQuery($query);
+			$rs = $this->_conn->FetchResult();
+			$this->username = $rs['username'];
+			$this->email = $rs['email'];
+			$this->email_confirmed = phpbool($rs['email_confirmed']);
+			$this->blocked = phpbool($rs['blocked']);
+			$this->reputation = $rs['reputation'];
+			$this->note = $rs['note'];
+			$this->created = $rs['created'];
+			$this->timestamp = $rs['timestamp'];
+		}
+	}
+
+	private function _update(){
+		if(!empty($this->id)){
+			$set = "SET userid=userid ";
+
+			if(!empty($this->username))
+				$set .= sprintf(", username='%s' ", $this->username);
+
+			if(!empty($this->email))
+				$set .= sprintf(", email='%s' ", $this->email);
+
+			if(!empty($this->email_confirmed))
+				$set .= sprintf(", email_confirmed='%s' ", pgbool($this->email_confirmed));
+
+			if(!empty($this->blocked))
+				$set .= sprintf(", blocked='%s' ", pgbool($this->blocked));
+
+			if(!empty($this->reputation))
+				$set .= sprintf(", reputation=%d ", $this->reputation);
+
+			if(!empty($this->note))
+				$set .= sprintf(", note='%s' ", $this->note);
+
+			$query = sprintf("UPDATE public.user %s WHERE userid=%d", $set, $this->id);
+			$this->_conn->ExecQuery($query);
+		}
+	}
+
+	function Save(){
+		if(!empty($this->id))
+			$this->_update();
+		else{
+			$this->id = $this->_insert();
+			$this->_get();
+			return $this->id;
+		}
+	}
+
+	function Delete(){
+		if(!empty($this->id)){
+			$query = sprintf("DELETE FROM public.user WHERE userid=%d", $this->id);
+			$this->_conn->ExecQuery($query);
+			unset($this->id);
+			unset($this->username);
+			unset($this->_password);
+			unset($this->email);
+			unset($this->email_confirmed);
+			unset($this->blocked);
+			unset($this->reputation);
+			unset($this->note);
+			unset($this->created);
+			unset($this->timestamp);
+		}
+	}
+}
+
+?>
